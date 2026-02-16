47ac85e9abeb2578decd5a7beb7dd936bf35e4ec
cassildobez2-star <cassildobez2@gmail.com>
2026-02-16 20:05:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
 
-env_vars = {
-    "API_ID": os.environ.get("API_ID"),
-    "API_HASH": os.environ.get("API_HASH"),
-    "BOT_TOKEN": os.environ.get("BOT_TOKEN")
-}
+API_ID = int(os.environ["API_ID"])
+API_HASH = os.environ["API_HASH"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
