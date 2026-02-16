a7bc0a16236c3f696be52ecfef6fdd16b0cc46f5
cassildobez2-star <cassildobez2@gmail.com>
2026-02-16 20:09:44 +0200

    Delete tools directory

--- tools/aqueue.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-
-class AQueue(asyncio.Queue):
-    async def put(self, item, user_id=0):
-        await super().put((item, user_id))
-
-    def qsize(self):
-        return super().qsize()
